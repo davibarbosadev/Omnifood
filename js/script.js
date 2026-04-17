a0f46241591d83c2443309f0de1306dfdeb98761
Davi barbosa <davibar26s@gmail.com>
2026-04-17 08:01:45 -0300

    feat: adicionar menu mobile e scroll suave

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const navToggle = document.querySelector(".nav__toggle");
+const nav = document.querySelector(".nav");
+const yearElement = document.querySelector("#year");
+
+navToggle.addEventListener("click", () => {
+    nav.classList.toggle("nav--open");
+    console.log("clicou");
+});
+
+const allLinks = document.querySelectorAll("a:any-link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+        console.log(href);
+
+        // scroll para voltar para o topo
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+
+            // scroll para outras seções
+        } else if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        // fechar menu mobile
+        if (link.classList.contains("nav__link")) {
+            nav.classList.remove("nav--open");
+        }
+    });
+});
+
+//////////////////////////////////////////////////////////////////
+// fixar(grudar) header na viewport
+const sectionHeroEl = document.querySelector(".section--hero");
+
+const observer = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        console.log(ent);
+        console.log("seção hero sumiu!!!");
+
+        if (ent.isIntersecting === false) {
+            document.querySelector(".header").classList.add("sticky");
+
+        } else if (ent.isIntersecting === true) {
+            document.querySelector(".header").classList.remove("sticky");
+        }
+    },
+    {
+        root: null,
+        threshold: 0,
+        rootMargin: "-64px",
+    },
+);
+
+observer.observe(sectionHeroEl);
+
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
